9e650001b99429fbeb788882790676d32806a0cb
Roman <roman@riflmedia.com>
2016-12-24 18:03:02 +0200

    add forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -30,11 +30,12 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     ShoppingListAddComponent,
     DropdownDirective,
     RecipeStartComponent,
-    RecipeEditComponent,
-  ],
+    RecipeEditComponent
+],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     routing
   ],
